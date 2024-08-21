4108ef125c820f413ca0ca6e9d133a129c2ec550
FeiraDoConhecimento <140769989+FeiraDoConhecimento@users.noreply.github.com>
2024-08-20 21:21:29 -0300

    4° commit

---
+++ src/assistir.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function Assistir(){
+    return (
+        <div>
+            <Header/>
+            <h1>Assistir</h1>
+            <p>Aqui você pode assistir aos seus videos favoritos.</p>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Assistir;
