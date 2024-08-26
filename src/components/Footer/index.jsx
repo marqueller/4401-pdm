a02aaf73d1e4fc12f90a5065cb8f745a14d12450
GustavoPatrici <167087120+GustavoPatrici@users.noreply.github.com>
2024-08-26 11:38:17 -0300

    Update index.jsx

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ import styles from "./Footer.module.css";
 function Footer() {
     return (
         <footer className={styles.footer}>
-            <h2>CedupFlix &copy; Desenvolvido por "Cristofer" 2024</h2>
+            <h2>CedupFlix &copy; Desenvolvido por "Gustavo Patricio" 2024</h2>
         </footer>
     );
 
