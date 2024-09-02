63f17e25897010b434e48e69a2c05125659e13e9
Kauan Albano <kauaninfo24@gmail.com>
2024-09-02 11:37:36 -0300

    4° commit

--- 1626-cedupflix/src/components/Footer/index.jsx
+++ 1626-cedupflix/src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ import styles from "./Footer.module.css";
 function Footer() {
     return (
         <footer className={styles.footer}>
-            <h2>CedupFlix &copy; Desenvolvido por Henrique 2024</h2>
+            <h2>CedupFlix &copy; Desenvolvido por Albano 2024</h2>
         </footer>
     );
 
