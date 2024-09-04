a7268a7a484640f9339530d52cc095ddc1940466
hyunshikk <contatosrgabrielp@gmail.com>
2024-09-04 17:05:33 -0300

    4º commit

---
+++ src/acesso.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function Acesso() {
+    return (
+        <div>
+            <Header />
+            <h1>LOGIN</h1>
+            <p>Aqui é onde você entra no coiso que faz você ser coiso</p>
+
+            <Footer />
+        </div>
+    );
+
+}
+
+export default Acesso;
---
+++ src/assistir.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function Assistir() {
+    return (
+        <div>
+            <Header />
+            <h1>Assista</h1>
+            <p>Aqui você pode assistir aos seus vídeos favoritos de Barbie</p>
+            <Footer />
+        </div>
+    );
+}
+
+export default Assistir;
