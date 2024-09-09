0598a4bfbfe9235a28a7564b00caddb90f731bda
Lucas Jeremias <140764081+HistoriaeInformatica@users.noreply.github.com>
2024-09-09 10:49:50 -0300

    4° commit

--- src/inicio.jsx
+++ src/inicio.jsx
@@ -4,7 +4,7 @@ function Home() {
     return (
         <div>
             <h1>Bem-Vindo à Home!</h1>
-            <p>Esta é a página inicial da aplicação.</p>
+            <p>Esta é a página inicial da aplicação!</p>
         </div>
     );
 }
